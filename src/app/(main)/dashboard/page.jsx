e90a429abf354ad5328c4b08c630b06cc8d80e38
Pankaj Kumar <94382374+Py8050147@users.noreply.github.com>
2025-02-01 00:33:53 +0530

    first commit

---
+++ src/app/(main)/dashboard/page.jsx
@@ -0,0 +1,23 @@
+import { getIndustryInsights } from "@/action/dashboard";
+import DashboardView from "./_components/dashboard-view";
+import OnboardingPage from "../onboarding/page";
+import { getUserOnboardingStatus } from "@/action/user";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  const insights = await getIndustryInsights();
+
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
+}
