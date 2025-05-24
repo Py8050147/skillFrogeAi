4fa483507c47ec1846f899c620eac53b429d261c
Pankaj Kumar <94382374+Py8050147@users.noreply.github.com>
2025-05-25 01:35:07 +0530

    first commit

--- src/app/lib/schema.js
+++ src/app/lib/schema.js
@@ -68,5 +68,5 @@ export const resumeSchema = z.object({
 export const coverLetterSchema = z.object({
   companyName: z.string().min(1, "Company name is required"),
   jobTitle: z.string().min(1, "Job title is required"),
-  jobDescription: z.string().min(1, "Job description is required"),
+  jobDescription: z.string().min(1, "Job description is required")
 });
